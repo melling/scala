ad939f1af780037d88ce41b820a1666c7966487e
Michael Mellinger <mmellinger66@gmail.com>
2019-07-26 12:39:01 -0400

    fixed deprecation

--- CmdLine.scala
+++ CmdLine.scala
@@ -1,11 +1,15 @@
 /*
+ * scalac -d classes CmdLine.scala
+ *
  * scala -classpath classes  Main
+ * scala -classpath classes  Main -h
+ * scala -classpath classes  Main -verbose
  */
 
 object Main {
   var verbose = false
 
-  def main(args: Array[String]) {
+  def main(args: Array[String]):Unit = {
     for (a <- args) a match {
       case "-h" | "-help"    =>
         println("Usage: scala Main [-help|-verbose]")
